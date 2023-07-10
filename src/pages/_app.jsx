cf7316e83541dfe6df32cce1555bfbc5d0a6bca1
minhluc073 <nguyenminhluc0703@gmail.com>
2023-07-10 17:01:13 +0700

    init project

---
+++ src/pages/_app.jsx
@@ -0,0 +1,13 @@
+import "../styles/index.scss";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <h1>
+        <p>hhhhhhhhhhhhhhagshdjasfasfghajsasfhgf</p>{" "}
+      </h1>
+    </>
+  );
+}
+
+export default MyApp;
---
+++ src/pages/_document.jsx
@@ -0,0 +1,20 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <link rel="shortcut icon" href="/assets/favicon.png" />
+      <link rel="preconnect" href="https://fonts.bunny.net" />
+      <link
+        href="https://fonts.bunny.net/css?family=outfit:100,200,300,400,500,600,700|playfair-display:400,400i,500,500i,600,600i,700,700i,800,800i|plus-jakarta-sans:400,400i,500,500i,600,600i,700,700i,800,800i"
+        rel="stylesheet"
+      />
+      <body>
+        <Main />
+        <h1>xxxxxxxxxxxxxxxxxxxxxxxxxxllllllllllllllll</h1>
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
